0507929a534de55891b6c192ddaa25d23c282894
Laytan Laats <laytanlaats@hotmail.com>
2022-09-14 12:22:14 +0200

    some null-ls config

--- nvim/.config/nvim/after/plugin/null-ls.lua
+++ nvim/.config/nvim/after/plugin/null-ls.lua
@@ -20,27 +20,39 @@ local setup_format_on_save = function(client, bufnr)
   end
 end
 
+local local_composer = { prefer_local = 'vendor/bin' }
+local fmt_no_code = { diagnostics_format = '[#{s}] #{m}' }
+local diag_on_save = { method = null.methods.DIAGNOSTICS_ON_SAVE }
+
 null.setup(
   {
+    debug = true,
     root_dir = utils.root_pattern('.null-ls-root', 'Makefile'),
     on_attach = setup_format_on_save,
+    diagnostics_format = '[#{s}] #{m} (#{c})',
     sources = {
       -- Diagnostics
       --
       -- General
       diagnostics.trail_space,
       -- PHP
-      diagnostics.phpstan,
-      diagnostics.phpcs,
-      diagnostics.phpmd,
+      diagnostics.phpstan.with(local_composer).with(fmt_no_code),
+      diagnostics.phpcs.with(local_composer).with(diag_on_save).with(
+        { extra_args = { '--cache' } }
+      ),
+      diagnostics.phpmd.with(local_composer).with(diag_on_save).with(
+        { extra_args = { 'phpmd.xml' } }
+      ),
       -- Twig
-      diagnostics.twigcs,
+      diagnostics.twigcs.with(local_composer).with(fmt_no_code).with(
+        { extra_args = { '--twig-version', '2' } }
+      ),
       -- GO
       diagnostics.golangci_lint,
       -- Lua
       diagnostics.selene,
       -- JS
-      diagnostics.eslint_d.with({ extra_filetypes = { 'yml' } }),
+      diagnostics.eslint_d.with({ extra_filetypes = { 'yaml' } }),
       diagnostics.tsc,
       -- Docker
       diagnostics.hadolint,
@@ -48,17 +60,28 @@ null.setup(
       -- Formatting
       --
       -- PHP
-      formatting.phpcbf,
+      formatting.phpcbf.with(local_composer),
       -- GO
       formatting.gofumpt,
-      formatting.goimports_reviser,
+      formatting.goimports_reviser.with(
+        {
+          args = {
+            '-file-path',
+            'FILENAME',
+            '-output',
+            'stdout',
+            '-set-alias',
+            '-rm-unused',
+          },
+        }
+      ),
       formatting.golines,
       -- Python
       formatting.isort,
       -- Lua
       formatting.lua_format,
       -- JS
-      formatting.eslint_d,
+      formatting.eslint_d.with({ extra_filetypes = { 'yaml' } }),
       -- JSON
       formatting.fixjson,
 
