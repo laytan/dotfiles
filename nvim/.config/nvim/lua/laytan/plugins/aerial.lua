04f789f986792377b38ba25adad8280e74c99e30
Laytan Laats <laytanlaats@hotmail.com>
2022-12-28 22:07:35 +0100

    fix: first run of aerial not working

--- nvim/.config/nvim/lua/laytan/plugins/aerial.lua
+++ nvim/.config/nvim/lua/laytan/plugins/aerial.lua
@@ -3,20 +3,28 @@ return {
   'stevearc/aerial.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   keys = { '<C-r>', '<C-t>', '<leader>fo' },
-  config = {
-    on_attach = function(bufnr)
+  config = function()
+    local on_attach = function(bufnr)
       vim.api.nvim_buf_set_keymap(
         bufnr, 'n', '<leader>fo', '<cmd>AerialToggle float<CR>', {}
       )
 
       vim.api
-        .nvim_buf_set_keymap(bufnr, 'n', '<C-r>', '<cmd>AerialPrev<CR>', {})
+          .nvim_buf_set_keymap(bufnr, 'n', '<C-r>', '<cmd>AerialPrev<CR>', {})
       vim.api
-        .nvim_buf_set_keymap(bufnr, 'n', '<C-t>', '<cmd>AerialNext<CR>', {})
-    end,
-    layout = { width = 45 },
-    highlight_on_jump = false,
-    close_on_select = true,
-    close_automatic_events = { 'switch_buffer' },
-  },
+          .nvim_buf_set_keymap(bufnr, 'n', '<C-t>', '<cmd>AerialNext<CR>', {})
+    end
+
+    require('aerial').setup(
+      {
+        on_attach = on_attach,
+        layout = { width = 45 },
+        highlight_on_jump = false,
+        close_on_select = true,
+        close_automatic_events = { 'switch_buffer' },
+      }
+    )
+
+    on_attach(vim.fn.bufnr())
+  end,
 }
